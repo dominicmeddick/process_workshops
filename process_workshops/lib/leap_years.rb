b3ca6475653c68c02236558fb1eaa7da2ac1c23f
Dominic Meddick <dominicmeddick@gmail.com>
2022-01-20 17:08:02 +0000

    all years divisible by 100 but not 400 are not leap years test passing

--- process_workshops/lib/leap_years.rb
+++ process_workshops/lib/leap_years.rb
@@ -3,6 +3,8 @@ class LeapYears
   def leap_year?(year)
     if year % 400 == 0
       return true
+    else year % 100 == 0 && year % 400 != 0
+      return false
     end
   end
 
--- process_workshops/spec/leap_years_spec.rb
+++ process_workshops/spec/leap_years_spec.rb
@@ -4,4 +4,8 @@ describe LeapYears do
   it 'returns true if divisble by 400' do
     expect(subject.leap_year?(2000)).to be(true)
   end
+
+  it 'returns false if divisible by 100 but not 400' do
+    expect(subject.leap_year?(1900)).to be(false)
+  end
 end
